8eefa6db1759ee6af21974571ea97e4718e87b4c
vlpekov <vladdey@gmail.com>
2017-10-11 16:14:40 +0300

    TimeOffController: save new request

--- src/main/java/com/tos/timeoffserver/domain/repositories/TimeOffRequestRepository.java
+++ src/main/java/com/tos/timeoffserver/domain/repositories/TimeOffRequestRepository.java
@@ -1,9 +1,11 @@
 package com.tos.timeoffserver.domain.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.tos.timeoffserver.domain.entites.TimeOffRequest;
 
 public interface TimeOffRequestRepository extends CrudRepository<TimeOffRequest, Long>{
-
+	List<TimeOffRequest> findAll();
 }
