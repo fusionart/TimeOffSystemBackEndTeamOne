2c38f266448e05d9546bf48b5c73b44f8b57b5e6
vlpekov <vladdey@gmail.com>
2017-10-12 15:54:30 +0300

    new entity: Document

---
+++ src/main/java/com/tos/timeoffserver/domain/entites/Document.java
@@ -0,0 +1,45 @@
+package com.tos.timeoffserver.domain.entites;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "documents")
+public class Document {
+
+	private Long documentId;
+	private String documentFileName;
+
+	public Document() {
+	}
+
+	public Document(String documentFileName) {
+		this.documentFileName = documentFileName;
+	}
+
+	@Id
+	@GeneratedValue
+	@Column(name = "document_id")
+
+	public Long getDocumentId() {
+		return documentId;
+	}
+
+	public void setDocumentId(Long documentId) {
+		this.documentId = documentId;
+	}
+
+	@Column(name = "file_name", nullable = false)
+
+	public String getDocumentFileName() {
+		return documentFileName;
+	}
+
+	public void setDocumentFileName(String documentFileName) {
+		this.documentFileName = documentFileName;
+	}
+}
